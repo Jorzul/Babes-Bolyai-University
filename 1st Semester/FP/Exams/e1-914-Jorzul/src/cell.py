f70bad2acc201ee269b6d7bb7bb7e29858e456dd
Jorza Ionut <115074953+Jorzul@users.noreply.github.com>
2024-03-01 16:31:57 +0200

    Delete 1st Semester/FP/Exams/e1-914-Jorzul directory

--- 1st Semester/FP/Exams/e1-914-Jorzul/src/cell.py
+++
@@ -1,8 +0,0 @@
-class Cell:
-    def __init__(self, row, col):
-        self.row = row
-        self.column = col
-        self.item = " "
-
-    def __str__(self):
-        return str(self.item)
--- 1st Semester/FP/Exams/e1-914-Jorzul/src/start.py
+++
@@ -1,3 +0,0 @@
-from src.UI import menu
-
-menu()
